4a1ed892cb46d90a5a96684a331daf8a9f45b0e1
Linhdieu <linh.mtd2416807@sis.hust.edu.vn>
2026-09-07 16:28:28 +0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -6,18 +6,21 @@ print("Here is the word:")
 for _ in range(len(word)):
     print("_", end = " ")
 guessed = []
-max_guess = 6
-
-for i in range(max_guess):
+guessed_wrong = []
+wrong_guess = 0
+while(wrong_guess < 6):
     display = ''
     letter = input("\nGuess a letter: \n").lower()
+    if(letter == '' or letter not in 'abcdefghijklmnopqrstuvwxyz' or len(letter) != 1):
+        print("Invalid input. Please enter another one.")
+        continue
     guessed.append(letter)
     if letter in word:
         print("\nCorrect guess!")
     else:
         print("\nIncorrect guess!")
-        max_guess -= 1
-
+        wrong_guess += 1
+        guessed_wrong.append(letter)
     for letter in word:
         if letter in guessed:
             display += letter + ' '
@@ -26,10 +29,13 @@ for i in range(max_guess):
     if(len(display.replace('_','').replace(' ','')) == len(word)):
             print("Win! The word was:",word)
             break
-    if(max_guess == 0):
+    if(wrong_guess == 6):
         print("Loose! The word was:", word)
+        print("Incorrect guesses:", guessed_wrong)
         break
-    if(max_guess>0):
+    if(wrong_guess < 6):
         print("\nCurrent word:\n", display)
+        print("Guessed letters:", guessed)
+        print('Guess times left:',6 - wrong_guess)
     
 file.close()
